7a2f6f967915961f34ceea302b2ef4f46c551117
FreeSize83 <freesize83@gmail.com>
2025-08-27 15:44:28 +0300

    feat(chip8): implement basic emulateCycle opcodes
    Add handling for CLS, RET, jumps, calls, skips, register loads/additions, and I register setup

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -40,3 +40,63 @@ void Chip8::loadGame(const char* filename) {
 	file.close();
 }
 
+void Chip8::emulateCycle() {
+	uint16_t opcode = memory[PC] << 8 | memory[PC + 1];
+
+	uint16_t first = opcode & 0xF000;
+	uint16_t NNN = opcode & 0x0FFF;
+	uint8_t NN = opcode & 0x00FF;
+	uint8_t N = opcode & 0x000F;
+	uint8_t X = (opcode & 0x0F00) >> 8;
+	uint8_t Y = (opcode & 0x00F0) >> 4;
+
+	switch (first) {
+
+		case 0x0000:
+			switch (opcode)
+			{
+			case 0x00E0:
+				for (int i = 0; i < 64 * 32; i++)
+					display[i] = 0;
+				PC += 2;
+				break;
+			case 0x00EE:
+				PC = stack[--SP];
+				PC += 2;
+				break;
+			}
+			break;
+
+		case 0x1000: 
+			PC = NNN;
+			break;
+	
+		case 0x2000: 
+			stack[SP++] = PC;
+			PC = NNN;
+			break;
+	
+		case 0x3000: 
+			PC += (V[X] == NN) ? 4 : 2;
+			break;
+	
+		case 0x6000:
+			V[X] = NN;
+			PC += 2;
+			break;
+	
+		case 0x7000:
+			V[X] += NN;
+			PC += 2;
+			break;
+		case 0xA000:
+			I = NNN;
+			PC += 2;
+			break;
+		case 0xD000:
+			PC += 2;
+			break;
+		default:
+			PC += 2;
+			break;
+}
