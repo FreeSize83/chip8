154499e03688cdf3e73ee1f28e09ccb1da232d07
FreeSize83 <freesize83@gmail.com>
2025-08-27 03:16:00 +0300

    feat: implement chip8 reset() method
    initialize memory, registers, stack, display, and timers to zero to ensure the emulator starts in clean state

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,1 +1,25 @@
-﻿
+﻿#include "../include/Chip8.h"
+
+Chip8::Chip8() {
+	reset();
+}
+
+void Chip8::reset() {
+		for (int i = 0; i < 4096; i++){
+			memory[i] = 0;
+		}
+		for (int i = 0; i < 16; i++) {
+			V[i] = 0;
+		}
+		for (int i = 0; i < 16; i++) {
+			stack[i] = 0;
+		}
+		for (int i = 0; i < 2048; i++) {
+			display[i] = 0;
+		}
+		I = 0;
+		PC = 0;
+		SP = 0;
+		delayTimer = 0;
+		soundTimer = 0;
+}
